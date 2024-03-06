ab49e105d6a3754dcb9466685020378b62e075e0
José Henrique Roveda <46685746+josehenriqueroveda@users.noreply.github.com>
2024-03-06 16:08:15 -0300

    chore: create example.py
    Co-authored-by: jhrov<josehenriqueroveda@gmail.com>

---
+++ agro_news/example.py
@@ -0,0 +1,33 @@
+from sentiment_analyzer import SentimentAnalyzer
+
+
+news = [
+    "Guerra comercial entre EUA e China pode piorar o mercado de soja",
+    "Fertilizantes estão mais caros e isso será ruim para produtores de grãos",
+    "Agrishow tem a melhor perspectiva de negócios dos últimos anos",
+    "Produção de etanol de milho pode servir de alternativa para produtores",
+]
+
+sa = SentimentAnalyzer(task="sentiment", lang="pt")
+
+print('##### Sentiment Analysis #####')
+for item in news:
+    print(item)
+    print(sa.analyze(item))
+    print('---------------------------------')
+
+  
+# Output:
+# ##### Sentiment Analysis #####
+# Guerra comercial entre EUA e China pode piorar o mercado de soja
+# AnalyzerOutput(output=NEU, probas={NEU: 0.616, NEG: 0.369, POS: 0.015})
+# ---------------------------------
+# Fertilizantes estão mais caros e isso será ruim para produtores de grãos
+# AnalyzerOutput(output=NEG, probas={NEG: 0.506, NEU: 0.487, POS: 0.007})
+# ---------------------------------
+# Agrishow tem a melhor perspectiva de negócios dos últimos anos
+# AnalyzerOutput(output=POS, probas={POS: 0.948, NEU: 0.047, NEG: 0.005})
+# ---------------------------------
+# Produção de etanol de milho pode servir de alternativa para produtores
+# AnalyzerOutput(output=NEU, probas={NEU: 0.945, POS: 0.036, NEG: 0.020})
+# ---------------------------------
